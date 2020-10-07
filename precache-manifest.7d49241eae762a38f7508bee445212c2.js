3edae1e8585ad7436af44f5baf8388f556d03a9a
jmonteer <jmonteer@protonmail.com>
2020-10-08 00:57:49 +0200

    Updates

---
+++ precache-manifest.7d49241eae762a38f7508bee445212c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3203fb5052d49ded91571f517ab4f9a1",
+    "url": "/hegic-pooled-staking-front/index.html"
+  },
+  {
+    "revision": "7c0c4e21b675419e6f79",
+    "url": "/hegic-pooled-staking-front/static/css/main.b4a6cc93.chunk.css"
+  },
+  {
+    "revision": "4a3620a873610f736927",
+    "url": "/hegic-pooled-staking-front/static/js/2.2f702fb0.chunk.js"
+  },
+  {
+    "revision": "be03aa230b0b7031723bbd2748ca0bf5",
+    "url": "/hegic-pooled-staking-front/static/js/2.2f702fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0c4e21b675419e6f79",
+    "url": "/hegic-pooled-staking-front/static/js/main.e9b8adf4.chunk.js"
+  },
+  {
+    "revision": "31b23e154fae1e963d50",
+    "url": "/hegic-pooled-staking-front/static/js/runtime-main.5d19f53a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hegic-pooled-staking-front/precache-manifest.7d49241eae762a38f7508bee445212c2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hegic-pooled-staking-front/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
