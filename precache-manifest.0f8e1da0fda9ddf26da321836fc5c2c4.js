928ab6ba6babea86a0ff9e6e2154e0683bb73f95
jmonteer <jmonteer@protonmail.com>
2020-10-28 18:57:30 +0100

    Updates

---
+++ precache-manifest.0f8e1da0fda9ddf26da321836fc5c2c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29f1208d7cf2dcb8b1e713b1f7bbaf2",
+    "url": "/hegic-staking-pool-front/index.html"
+  },
+  {
+    "revision": "bccef51c9d3945b82f7e",
+    "url": "/hegic-staking-pool-front/static/css/main.259fcf8d.chunk.css"
+  },
+  {
+    "revision": "e7d2edd65c1522a41d15",
+    "url": "/hegic-staking-pool-front/static/js/2.f33bee44.chunk.js"
+  },
+  {
+    "revision": "be03aa230b0b7031723bbd2748ca0bf5",
+    "url": "/hegic-staking-pool-front/static/js/2.f33bee44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bccef51c9d3945b82f7e",
+    "url": "/hegic-staking-pool-front/static/js/main.21ac0f66.chunk.js"
+  },
+  {
+    "revision": "daf5d9a10634c0e21849",
+    "url": "/hegic-staking-pool-front/static/js/runtime-main.8188c419.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e45a3f8316a96ea4e051146da54e28d3.js"
+  "/hegic-staking-pool-front/precache-manifest.0f8e1da0fda9ddf26da321836fc5c2c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hegic-staking-pool-front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
